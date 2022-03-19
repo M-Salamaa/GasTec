e8999772e8094685d4830a60f547420ad742db57
M-Salamaa <98873801+M-Salamaa@users.noreply.github.com>
2022-03-19 15:21:54 +0200

    Search Result

---
+++ WebApp_gastec/WebApp_gastec/Models/OutputSearchModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApp_gastec
+{
+    public class OutputSearchModel
+    {
+        public List<SearchClassFicationTreeModel> ListClassFicationTree { get; set; }
+        public List<SearchListNewsModel> ListNews { get; set; }
+        public List<SearchListVideosModel> ListVideos { get; set; }
+    }
+}
---
+++ WebApp_gastec/WebApp_gastec/Models/ViewModels/SearchInputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApp_gastec.Models
+{
+    public class SearchInputModel
+    {
+        public string keyWord_ { get; set; }
+    }
+}
